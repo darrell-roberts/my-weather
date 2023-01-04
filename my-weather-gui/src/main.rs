c0350ca225b52eaaaae799843c834e989dd6482f
Darrell Roberts <droberts0674@gmail.com>
2023-01-04 12:20:18 -0500

    update for mac/linux rendering differences

--- my-weather-gui/src/main.rs
+++ my-weather-gui/src/main.rs
@@ -2,7 +2,12 @@ use my_weather_gui::AppModel;
 use relm4::RelmApp;
 
 fn main() {
-  let app = RelmApp::new(AppModel::default());
-  relm4::set_global_css(include_bytes!("style.css"));
-  app.run();
+    let app = RelmApp::new(AppModel::default());
+    #[cfg(target_os = "macos")]
+    relm4::set_global_css(include_bytes!("style_mac.css"));
+
+    #[cfg(not(target_os = "macos"))]
+    relm4::set_global_css(include_bytes!("style.css"));
+
+    app.run();
 }
